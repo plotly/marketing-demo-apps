093dafa4d6d92d2f216232eeb8e36d047342c4a7
A Beresford <aberesf@gmail.com>
2024-03-27 10:40:46 -0400

    requirements and imports

--- dash-usa-govt-analytics/app.py
+++ dash-usa-govt-analytics/app.py
@@ -1,8 +1,6 @@
 import dash
 import dash_design_kit as ddk
-from dash.dependencies import Input, Output
-import dash_html_components as html
-import dash_core_components as dcc
+from dash import Input, Output, html, dcc
 
 import data
 
