99a19a878d33beeb10bb0df0f134168c3f02b9c2
A Beresford <aberesf@gmail.com>
2024-04-18 11:31:53 -0400

    mapbox

--- dash-uber-ride/app.py
+++ dash-uber-ride/app.py
@@ -18,7 +18,7 @@ server = app.server
 
 
 # Plotly mapbox public token. Note that this token is URL locked to the services server, and will not work from your local machine. You can get a tiling plan from Mapbox here: https://www.mapbox.com/. You will need a (free) public token.
-mapbox_access_token = "pk.eyJ1IjoicGxvdGx5LWRvY3MiLCJhIjoiY2xzejNpNHBlMDhpeDJrbHBvZzQ4ZmlzNyJ9.RB6OFUu460lCOxJei9LadA"
+mapbox_access_token = 'pk.eyJ1IjoiamFja2x1byIsImEiOiJjaXhzYTB0bHcwOHNoMnFtOWZ3YWdreDB3In0.pjROwb9_CEuyKPE-x0lRUw'
 
 
 # Dictionary of important locations in New York
